cca1efefc13868554c442ab8f140ae00b979d94c
Matthew Willard <MWillard85@gmail.com>
2019-10-03 12:45:06 -0600

    updated deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
     //API_ENDPOINT: 'INSERT HEROKU',
-    API_ENDPOINT: "http://localhost:8000/api",
+    API_ENDPOINT: "https://ancient-basin-53352.herokuapp.com/api",
     TOKEN_KEY: "training-client-auth-token"
   };
